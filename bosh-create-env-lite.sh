1eff9e63743051f42823417e21bf635c10982869
Toshiaki Maki <tmaki@pivotal.io>
2017-10-25 21:19:53 +0900

    add bosh-create-env-lite.sh

---
+++ bosh-create-env-lite.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+bosh create-env ../bosh-deployment/bosh.yml \
+  --state ../state.json \
+  -o ../bosh-deployment/virtualbox/cpi.yml \
+  -o ../bosh-deployment/virtualbox/outbound-network.yml  \
+  -o ../bosh-deployment/bosh-lite.yml \
+  -o ../bosh-deployment/bosh-lite-runc.yml \
+  -o ../bosh-deployment/uaa.yml \
+  -o ../bosh-deployment/credhub.yml \
+  -o ../bosh-deployment/jumpbox-user.yml \
+  -o ./ops-files/director-size-lite.yml \
+  --vars-store ./creds.yml \
+  -v director_name=bosh-lite \
+  -v internal_ip=192.168.50.6 \
+  -v internal_gw=192.168.50.1 \
+  -v internal_cidr=192.168.50.0/24 \
+  -v outbound_network_name=NatNetwork \
+  -o external-ip-not-recommended.yml \
+  -o external-ip-not-recommended-uaa.yml \
+  -o external-ip-not-recommended-credhub.yml \
+  -v external_ip=202.241.169.198
