1f88a941ae00c0060217f64fde8adf6ebccab21e
Toshiaki Maki <tmaki@pivotal.io>
2017-12-18 01:43:14 +0900

    Support Credhub

--- deploy-concourse.sh
+++ deploy-concourse.sh
@@ -4,8 +4,10 @@ bosh deploy -d concourse concourse-deployment/cluster/concourse.yml \
      -l concourse-deployment/versions.yml \
      -o concourse-deployment/cluster/operations/static-web.yml \
      -o concourse-deployment/cluster/operations/basic-auth.yml \
-     -o concourse-deployment/cluster/operations/external-postgres.yml \
      -o ops-files/concourse-credhub.yml \
+     -o ops-files/concourse-colocated-credhub.yml \
+     -o concourse-deployment/cluster/operations/external-postgres.yml \
+     -o ops-files/concourse-colocated-credhub-external-postgres.yml \
      -o ops-files/concourse-variables.yml \
      -v web_ip=10.244.1.120 \
      -v external_url=https://concourse.ik.am \
@@ -18,7 +20,11 @@ bosh deploy -d concourse concourse-deployment/cluster/concourse.yml \
      -v postgres_host=192.168.220.40 \
      -v postgres_port=5432 \
      -v postgres_role=atc \
-     -v credhub_url=https://192.168.50.6:8844 \
+     -v credhub_url=https://10.244.1.100:8844 \
+     -v credhub-ip=10.244.1.100 \
+     -v uaa-url=https://192.168.50.6:8443 \
+     --var-file uaa-tls.ca=<(bosh int bosh-lite-creds.yml --path /uaa_ssl/ca) \
+     --var-file uaa-jwt.public_key=<(bosh int bosh-lite-creds.yml --path /uaa_jwt_signing_key/public_key) \
      --var-file credhub_tls.ca=<(bosh int ~/bosh-deployment/creds.yml --path /credhub_ca/ca) \
      -v credhub_client_id=director_to_credhub \
      -v credhub_client_secret=`bosh int ~/bosh-deployment/creds.yml --path /uaa_clients_director_to_credhub` \
