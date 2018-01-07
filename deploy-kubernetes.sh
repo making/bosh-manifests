f064f4eab86f3e7f13e63c48d1c66a3386a6cc4d
Toshiaki Maki <tmaki@pivotal.io>
2018-01-07 14:54:01 +0900

    Add cfcr

--- deploy-kubernetes.sh
+++ deploy-kubernetes.sh
@@ -3,8 +3,10 @@ set -e
 
 bosh deploy -d cfcr kubo-deployment/manifests/cfcr.yml \
             -o ops-files/use-specific-stemcell.yml \
+            -o ops-files/kubernetes-kubo-0.11.0.yml \
             -o ops-files/kubernetes-static-ips.yml \
+            -o ops-files/kubernetes-single-worker.yml \
             -v stemcell_version="3468.13" \
             -v kubernetes_master_host=10.244.1.92 \
-            -v kubernetes_worker_hosts='["10.244.1.93","10.244.2.93","10.244.3.93"]'
+            -v kubernetes_worker_hosts='["10.244.1.93"]' \
             --no-redact
