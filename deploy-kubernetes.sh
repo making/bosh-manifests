a4c30138beb2df914f8e006ab127e0c6ed668e5d
Toshiaki Maki <tmaki@pivotal.io>
2018-02-22 03:43:27 +0900

    add kubernetes-add-alternative-name

--- deploy-kubernetes.sh
+++ deploy-kubernetes.sh
@@ -7,7 +7,9 @@ bosh deploy -d cfcr kubo-deployment/manifests/cfcr.yml \
             -o ops-files/kubernetes-static-ips.yml \
             -o ops-files/kubernetes-single-worker.yml \
             -o ops-files/kubernetes-dummy-cloud-provider.yml \
+            -o ops-files/kubernetes-add-alternative-name.yml \
             -v stemcell_version="3468.21" \
             -v kubernetes_master_host=10.244.1.92 \
             -v kubernetes_worker_hosts='["10.244.1.93"]' \
+            -v add_alternative_name=gmo.ik.am \
             --no-redact
