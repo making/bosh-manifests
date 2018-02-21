990755f2336cb26002c1417ae9d0934a420fde57
Toshiaki Maki <tmaki@pivotal.io>
2018-02-22 03:46:33 +0900

    Bump to CFCR 0.14.0

--- deploy-kubernetes.sh
+++ deploy-kubernetes.sh
@@ -3,10 +3,9 @@ set -e
 
 bosh deploy -d cfcr kubo-deployment/manifests/cfcr.yml \
             -o ops-files/use-specific-stemcell.yml \
-            -o ops-files/kubernetes-kubo-0.13.0.yml \
+            -o ops-files/kubernetes-kubo-0.14.0.yml \
             -o ops-files/kubernetes-static-ips.yml \
             -o ops-files/kubernetes-single-worker.yml \
-            -o ops-files/kubernetes-dummy-cloud-provider.yml \
             -o ops-files/kubernetes-add-alternative-name.yml \
             -v stemcell_version="3468.21" \
             -v kubernetes_master_host=10.244.1.92 \
