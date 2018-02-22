2e41305c10e3eaa31f7bc739ca41a050d4e684c6
Toshiaki Maki <tmaki@pivotal.io>
2018-02-23 03:32:53 +0900

    Support OIDC

--- deploy-kubernetes.sh
+++ deploy-kubernetes.sh
@@ -7,8 +7,13 @@ bosh deploy -d cfcr kubo-deployment/manifests/cfcr.yml \
             -o ops-files/kubernetes-static-ips.yml \
             -o ops-files/kubernetes-single-worker.yml \
             -o ops-files/kubernetes-add-alternative-name.yml \
+            -o ops-files/kubernetes-oidc.yml \
+            -o ops-files/kubernetes-oidc-username.yml \
             -v stemcell_version="3468.21" \
             -v kubernetes_master_host=10.244.1.92 \
             -v kubernetes_worker_hosts='["10.244.1.93"]' \
             -v add_alternative_name=gmo.ik.am \
+            -v oidc-issuer-url=https://keycloak.ik.am/auth/realms/home \
+            -v oidc-client-id=kubernetes \
+            -v oidc-username-claim=preferred_username \
             --no-redact
