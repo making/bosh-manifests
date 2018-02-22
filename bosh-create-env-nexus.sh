e7c93c8cd0f14d3889cb0649df59b55d374f31a9
Toshiaki Maki <tmaki@pivotal.io>
2018-02-22 13:04:31 +0900

    Add datadog agent

--- bosh-create-env-nexus.sh
+++ bosh-create-env-nexus.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 bosh create-env nexus-standalone.yml \
+     -o ops-files/datadog-for-create-env.yml \
      -v internal_ip=192.168.230.40 \
      --vars-store ./nexus-creds.yml \
      --var-file nexus_ssl.certificate=~/sslip.io/sslip.io.crt \
      --var-file nexus_ssl.private_key=~/sslip.io/sslip.io.key \
+     -v datadog-hostname=nexus \
+     -v datadog-api-key=${DD_API_KEY} \
      --state=nexus-standalone-state.json
