a61a752a0678dc2ec5c8d8575c1365f579872fc0
Toshiaki Maki <tmaki@pivotal.io>
2018-03-30 02:35:50 +0900

    up

--- bosh-create-env-lite.sh
+++ bosh-create-env-lite.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 bosh create-env bosh-deployment/bosh.yml \
-  --state bosh-lite-state.json \
   -o bosh-deployment/virtualbox/cpi.yml \
   -o bosh-deployment/virtualbox/outbound-network.yml  \
   -o bosh-deployment/bosh-lite.yml \
@@ -10,10 +9,14 @@ bosh create-env bosh-deployment/bosh.yml \
   -o bosh-deployment/credhub.yml \
   -o bosh-deployment/jumpbox-user.yml \
   -o ops-files/director-size-lite.yml \
+  -o ops-files/datadog-for-create-env.yml \
   -o prometheus-boshrelease/manifests/operators/bosh/add-bosh-exporter-uaa-clients.yml \
   --vars-store bosh-lite-creds.yml \
   -v director_name=bosh-lite \
   -v internal_ip=192.168.50.6 \
   -v internal_gw=192.168.50.1 \
   -v internal_cidr=192.168.50.0/24 \
-  -v outbound_network_name=NatNetwork
+  -v outbound_network_name=NatNetwork \
+  -v datadog-hostname=bosh-lite \
+  -v datadog-api-key=${DD_API_KEY} \
+  --state bosh-lite-state.json
