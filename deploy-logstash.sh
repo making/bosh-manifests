f01b207c85de99adca9b3612c516ad87e0a7ec5d
Toshiaki Maki <tmaki@pivotal.io>
2018-03-30 02:41:26 +0900

    Add logstash

---
+++ deploy-logstash.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+bosh -d logstash deploy logstash-boshrelease/manifest/logstash.yml \
+     -o logstash-boshrelease/manifest/add-public-ip.yml \
+     -o logstash-boshrelease/manifest/add-tls.yml \
+     --var-file logstash.conf=logstash.conf \
+     -v external_ip=${EXTERNAL_IP} \
+     --var-file logstash_tls.certificate=${HOME}/gdrive/sslip.io/sslip.io.crt \
+     --var-file logstash_tls.private_key=${HOME}/gdrive/sslip.io/sslip.io.key \
+     --no-redact
