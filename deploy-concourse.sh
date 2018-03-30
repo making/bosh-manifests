c1a0c8af6a2f5698cbc89a36761aa582cf76f113
Toshiaki Maki <tmaki@pivotal.io>
2018-03-30 14:31:36 +0900

    Bump to Concouse 3.10.0

--- deploy-concourse.sh
+++ deploy-concourse.sh
@@ -5,8 +5,8 @@ bosh deploy -d concourse concourse-deployment/cluster/concourse.yml \
      -o concourse-deployment/cluster/operations/static-web.yml \
      -o concourse-deployment/cluster/operations/basic-auth.yml \
      -o concourse-deployment/cluster/operations/external-postgres.yml \
-     -o ops-files/use-specific-stemcell.yml \
      -o ops-files/concourse-emtpy-certs-path.yml \
+     -o ops-files/use-specific-stemcell.yml \
      -o prometheus-boshrelease/manifests/operators/concourse/enable-prometheus-metrics.yml \
      -v stemcell_version="3468.21" \
      -v web_ip=10.244.1.120 \
@@ -26,9 +26,8 @@ bosh deploy -d concourse concourse-deployment/cluster/concourse.yml \
      --var-file uaa-tls.ca=<(bosh int bosh-lite-creds.yml --path /uaa_ssl/ca) \
      --var-file uaa-jwt.public_key=<(bosh int bosh-lite-creds.yml --path /uaa_jwt_signing_key/public_key) \
      -v credhub_client_id=director_to_credhub \
-     -v credhub_client_secret=`bosh int ~/bosh-deployment/creds.yml --path /uaa_clients_director_to_credhub` \
+     -v credhub_client_secret=`bosh int bosh-lite-creds.yml --path /uaa_clients_director_to_credhub` \
      --no-redact
-
 
 # -o ops-files/concourse-credhub.yml \
 # -o ops-files/concourse-credhub-external-postgres.yml \
