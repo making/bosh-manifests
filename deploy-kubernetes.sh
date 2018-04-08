09034efcfc2db7eb60b2dc31ce544fe8ab287ad3
Toshiaki Maki <tmaki@pivotal.io>
2018-04-08 23:09:05 +0900

    Update cfcr

--- deploy-kubernetes.sh
+++ deploy-kubernetes.sh
@@ -2,18 +2,21 @@
 set -e
 
 bosh deploy -d cfcr kubo-deployment/manifests/cfcr.yml \
+            -o kubo-deployment/manifests/ops-files/add-oidc-endpoint.yml \
+            -o kubo-deployment/manifests/ops-files/system-specs.yml \
             -o ops-files/use-specific-stemcell.yml \
             -o ops-files/kubernetes-kubo-0.14.0.yml \
             -o ops-files/kubernetes-static-ips.yml \
             -o ops-files/kubernetes-single-worker.yml \
             -o ops-files/kubernetes-add-alternative-name.yml \
-            -o ops-files/kubernetes-oidc.yml \
-            -o ops-files/kubernetes-oidc-username.yml \
+            -o ops-files/kubernetes-remove-unused-oidc-properties.yml \
+            -o ops-files/kubernetes-remove-cloud-provider.yml \
             -v stemcell_version="3468.21" \
             -v kubernetes_master_host=10.244.1.92 \
             -v kubernetes_worker_hosts='["10.244.1.93"]' \
             -v add_alternative_name=gmo.ik.am \
-            -v oidc-issuer-url=https://keycloak.ik.am/auth/realms/home \
-            -v oidc-client-id=kubernetes \
-            -v oidc-username-claim=preferred_username \
+            -v oidc_issuer_url=https://keycloak.ik.am/auth/realms/home \
+            -v oidc_client_id=kubernetes \
+            -v oidc_username_claim=preferred_username \
+            -v authorization_mode=RBAC \
             --no-redact
