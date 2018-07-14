3ee22fa7534509e5fc1f0bcfcc81f5005133d5f4
Toshiaki Maki <tmaki@pivotal.io>
2018-07-14 15:36:21 +0000

    Update logstash

--- deploy-logstash.sh
+++ deploy-logstash.sh
@@ -1,15 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 bosh -d logstash deploy logstash-boshrelease/manifest/logstash.yml \
-     -o logstash-boshrelease/manifest/add-public-ip.yml \
-     -o logstash-boshrelease/manifest/add-tls.yml \
-     -o ops-files/logstash-change-vm-type.yml \
+     -o logstash-boshrelease/manifest/add-lb.yml \
+     -o logstash-boshrelease/manifest/instances.yml \
+     -o logstash-boshrelease/manifest/azs.yml \
+     -o logstash-boshrelease/manifest/types.yml \
+     -o logstash-boshrelease/manifest/readiness-probe.yml \
      -o ops-files/logstash-consume-elasticsearch.yml \
-     --var-file logstash.conf=logstash.conf \
-     -v external_ip=${EXTERNAL_IP} \
-     --var-file logstash_tls.certificate=${HOME}/gdrive/sslip.io/sslip.io.crt \
-     --var-file logstash_tls.private_key=${HOME}/gdrive/sslip.io/sslip.io.key \
-     --no-redact
-
-
-#     -o ops-files/logstash-release.yml \
+     -v logstash_instances=2 \
+     -v logstash_azs="[z1, z2, z3]" \
+     -v logstash_disk_type="5120" \
+     -v logstash_vm_type=minimal \
+     -v readiness_probe_http_port=0 \
+     -v readiness_probe_tcp_port=5514 \
+     -o <(cat <<EOF
+- type: replace
+  path: /instance_groups/name=logstash/vm_extensions?/-
+  value: spot-instance-t2.micro
+EOF) \
+--var-file logstash.conf=logstash.conf \
+--no-redact
