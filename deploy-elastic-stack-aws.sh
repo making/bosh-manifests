93413bff562540bbf4187ee03a943173c00e05e6
Toshiaki Maki <tmaki@pivotal.io>
2018-07-15 15:53:22 +0000

    Update elastic-stack-bosh-deployment

--- deploy-elastic-stack-aws.sh
+++ deploy-elastic-stack-aws.sh
@@ -26,21 +26,21 @@ bosh -d elastic-stack deploy elastic-stack-bosh-deployment/elastic-stack.yml \
   path: /instance_groups/name=logstash/vm_extensions?/-
   value: spot-instance-t2.micro
 EOF) \
-     -v elasticsearch_master_instances=3 \
+     -v elasticsearch_master_instances=1 \
      -v elasticsearch_master_vm_type=minimal \
      -v elasticsearch_master_disk_type=5120 \
      -v elasticsearch_master_network=default \
      -v elasticsearch_master_azs="[z1, z2, z3]" \
-     -v elasticsearch_data_instances=2 \
+     -v elasticsearch_data_instances=1 \
      -v elasticsearch_data_vm_type=minimal \
      -v elasticsearch_data_disk_type=5120 \
      -v elasticsearch_data_network=default \
      -v elasticsearch_data_azs="[z1, z2, z3]" \
-     -v logstash_instances=2 \
+     -v logstash_instances=1 \
      -v logstash_vm_type=minimal \
      -v logstash_disk_type=5120 \
      -v logstash_network=default \
      -v logstash_azs="[z1, z2, z3]" \
-     -v readiness_probe_http_port=0 \
-     -v readiness_probe_tcp_port=5514 \
+     -v logstash_readiness_probe_http_port=0 \
+     -v logstash_readiness_probe_tcp_port=5514 \
      --no-redact
