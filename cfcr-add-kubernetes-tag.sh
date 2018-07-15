f3def588912405a60b02a53255387e63bfa2e5ec
Toshiaki Maki <tmaki@pivotal.io>
2018-07-16 00:58:17 +0900

    remove kubo

--- cfcr-add-kubernetes-tag.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-for subnet_id in `aws ec2 describe-subnets --filters "Name=tag-key,Values=Name" "Name=tag-value,Values=bbl-env*" | jq -r '.[][].SubnetId'`;do
-	aws ec2 create-tags --resources ${subnet_id} --tags Key=KubernetesCluster,Value=cfcr
-done
-
